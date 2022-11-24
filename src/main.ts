5908cba02bd6d81a95487404978f9cd43a184a29
Lucas <lucas.soubry@gmail.com>
2022-11-24 10:57:36 +0100

    initialisation Form project

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import './style.css'
+
+function fibobbbb (index : number){
+  console.log(index)
+  for(let i=0;i<3;i++){
+    index += index * 2;
+    }
+  return index
+}
+
+console.log(fibobbbb(2))
+
+function aled (aa : string){
+  for(let i=0;i<3;i++){
+    aa += 2;
+    }
+  return aa
+}
+
+console.log(aled)
